7c973d986aacc5119127baaac762fd53afa38d1e
campusmacha2025 <guner_charly@hotmail.com>
2025-07-17 16:21:57 +0200

    Delete js/app.js

--- js/app.js
+++
@@ -1,6 +0,0 @@
-
-// Script base
-document.addEventListener("DOMContentLoaded", () => {
-  const cursos = document.getElementById("cursos");
-  cursos.innerHTML = "<p class='text-center col-span-3'>Aquí aparecerán los cursos pronto...</p>";
-});
